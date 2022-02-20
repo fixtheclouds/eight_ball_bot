a8ac74d0e3f06b52f2dd3e28ec2a363330255f6f
Egor Romanov <egor@republic.co>
2022-02-20 23:20:01 +0300

    Add initial implementation

--- src/eight_ball_bot.cr
+++ src/eight_ball_bot.cr
@@ -1,6 +1,24 @@
-# TODO: Write documentation for `EightBallBot`
-module EightBallBot
-  VERSION = "0.1.0"
+require "tourmaline"
 
-  # TODO: Put your code here
+# TODO: Write documentation for `EightBallBot`
+class EightBallBot < Tourmaline::Client
+  STATEMENTS = [
+    "Yes",
+    "Most likey",
+    "No",
+    "Never",
+    "Don`t count on it"
+  ]
+
+  @[Command("shake")]
+  def shake_command(ctx)
+    ctx.message.reply(random_statement)
+  end
+
+  private def random_statement
+    STATEMENTS.sample
+  end
 end
+
+bot = EightBallBot.new(bot_token: ENV["API_KEY"])
+bot.poll
